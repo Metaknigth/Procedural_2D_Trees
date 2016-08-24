e1d8ee730eb78e316c7fc6ffc5c7016e60520571
bg46ia <bg46ia@student.sunderland.ac.uk>
2016-08-24 19:16:22 +0100

    Fixed noise glitch

--- FractalBranch/sketch.js
+++ FractalBranch/sketch.js
@@ -3,16 +3,57 @@ var len = 100;
 var thickness = 10;
 var thicknessDegrade = 0.78;
 var count = 0;
+var seed = 22;
+var noiseValue = 0;
 //var degrees = 30;
-var noiseValue;
 var root, tree = [],
   leaves = [];
+
+//variables for debugging
+var noiseResult;
+var logLength, logAngle, logInclusion;
 
 //setup function that is called on runtime, part of the p5.js library
 //pretty much the same as window.onLoad = function(){}
 function setup() {
   createCanvas(800, 600); //manageable size on my laptop
 
+  initNoise(seed, noiseValue);
+  initTree();
+
+  // //using vectors, part of the p5.js library
+  // var a = createVector(width / 2, height);
+  // var b = createVector(width / 2, height - len);
+  //
+  // //create the root/axiom starting at a and ending at b with a thickness
+  // root = new Branch(a, b, thickness);
+  // //add it to the array
+  // tree[0] = root;
+  // //generate the rest of the array, then in Draw display it
+  // //generate the branches/tree
+  // addBranches();
+  // //generate the leaves at the end branches
+  // addLeaves();
+
+
+  //randomSeed(20);
+  //create some noise;
+
+  //noiseSeed(100);
+
+
+}
+
+// add branches on mouse button being pressed
+// function mousePressed() {
+//   addBranches();
+// 	redraw();
+// }
+
+function initTree() {
+  //clean the arrays, for mousePressed
+  tree.length = 0;
+  leaves.length = 0;
   //using vectors, part of the p5.js library
   var a = createVector(width / 2, height);
   var b = createVector(width / 2, height - len);
@@ -22,19 +63,28 @@ function setup() {
   //add it to the array
   tree[0] = root;
   //generate the rest of the array, then in Draw display it
+  //generate the branches/tree
   addBranches();
+  //generate the leaves at the end branches
   addLeaves();
-
-  //create some noise;
-  //noiseSeed(10);
-  noiseValue = noise(20);
 }
 
 // add branches on mouse button being pressed
-// function mousePressed() {
-//   addBranches();
-// 	redraw();
-// }
+function mousePressed() {
+  //addBranches();
+  noiseValue += 0.5;
+  initNoise(seed,noiseValue);
+  initTree();
+  redraw();
+  println("noiseValue = " + noiseValue);
+  println("noiseResult = " + noiseResult);
+  println("logAngle = " + logAngle);
+  println("logLength = " + logLength);
+  println("logInclusion = " + logInclusion);
+  println("tree.lenght = " + tree.length);
+  println("leaves.length = " + leaves.length);
+  println("tree[100].branchLength = " + tree[100].branchLength);
+}
 
 //substituting mousePressed in tutorials
 function addBranches() {
@@ -42,8 +92,8 @@ function addBranches() {
 
     for (var i = tree.length - 1; i >= 0; i--) {
       if (tree[i].branchLength > 5) {
-
-        if (!tree[i].finished) { //finished is like drawn = true; each element in tree having a start and end pint
+        //finished is like drawn = true; each element in tree having a start and end pint
+        if (!tree[i].finished) {
           tree.push(tree[i].growBranchLeft());
           tree.push(tree[i].growBranchRight());
         }
@@ -82,8 +132,9 @@ function draw() {
   //draw the leaves
   var leaveSize = 0;
   for (var i = 0; i < leaves.length; i++) {
-    fill('rgb( ' + getRandomIntInclusive(51, 255) + ', ' + getRandomIntInclusive(102, 255) + ',' +
-      getRandomIntInclusive(0, 0) + ' )');
+    fill('rgb( ' + Math.round(getRandomIntInclusive(51, 255)) + ', ' +
+      Math.round(getRandomIntInclusive(102, 255)) + ',' +
+      Math.round(getRandomIntInclusive(0, 0)) + ' )');
     noStroke();
     leaveSize = getRandomIntInclusive(4, 10);
     ellipse(leaves[i].x, leaves[i].y, leaveSize, leaveSize);
@@ -91,7 +142,25 @@ function draw() {
 
   //debugging
   fill('#000000');
-  text(noiseValue, 100,100);
+  //text(noiseValue, 100, 100);
+  text("1.0", 0, 50);
+  text("0.0(y=50)", 0, 100);
+  text("-1.0", 0, 150);
+
+  stroke('#222222');
+  strokeWeight(2);
+  //line(0, 100, width, 100);
+  noFill();
+  beginShape();
+  for (var x = 0; x < width; x++) {
+    var nx = map(x, 0, width, 0, 5);
+    var y = 100 * noise(nx);
+    vertex(x, y);
+  }
+  text(y, 100, 100);
+  endShape();
+
+
   noLoop(); // looping disabled, so it only draws it once
 }
 
@@ -103,16 +172,58 @@ function draw() {
 
 //for randomizing the lenth
 function lengthRandomizer() {
-  return getRandomIntInclusive(68, 84) / 100
+  logLength = getRandomIntInclusive(68, 84) / 100;
+  return logLength;
 }
 //get a random angle,
 function angleRandomizer() {
-  return getRandomIntInclusive(10, 30)
+  logAngle = getRandomIntInclusive(10, 30);
+  return logAngle;
 }
 
-function getRandomIntInclusive(min, max) {
-  return Math.round(Math.random() * (max - min + 1)) + min;
+function initNoise(seed, noiseValue) {
+  noiseSeed(this.seed);
+  noiseResult = noise(this.noiseValue);
+
 }
+
+// function getRandomIntInclusive(min, max) {
+//   //randomSeed(15124551); //works only with p5.js random()
+//   //random() for p5.js random and Math.random default javascript
+//   return Math.round(Math.random() * (max - min + 1)) + min;
+// }
+
+//noise version
+// function getRandomIntInclusive(min, max) {
+//   //randomSeed(15124551); //works only with p5.js random()
+//   //random() for p5.js random and Math.random default javascript
+//   //noiseSeed(20);
+//   //noiseValue = noiseSeedV;
+//   g++;
+//   randomValuesStorage[g] = noise(noiseValue);
+//   return (randomValuesStorage[g] * (max - min + 1)) + min;
+//
+// }
+
+function getRandomIntInclusive(min, max) {
+  //randomSeed(15124551); //works only with p5.js random()
+  //random() for p5.js random and Math.random default javascript
+  //return Math.round(Math.random() * (max - min + 1)) + min;
+
+  logInclusion = (noiseResult * (max - min + 1)) + min;
+  return logInclusion;
+}
+
+// function getRandomIntInclusive(min, max) {
+//   randomSeed(1512); //works only with p5.js random()
+//   //random() for p5.js random and Math.random default javascript
+//   //return Math.round(Math.random() * (max - min + 1)) + min;
+//   noiseResult = random();
+//   logInclusion = (noiseResult * (max - min + 1)) + min;
+//   return logInclusion;
+// }
+
+
 
 //for debugging purposes
 function debugBranches(treeBranch) {
